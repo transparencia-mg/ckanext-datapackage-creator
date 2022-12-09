c1ff4a6e892d0cef2faeb70b5840cc5c2f09432a
Gileno Filho <contato@gilenofilho.com.br>
2022-12-09 11:01:03 -0300

    adjusting fields in package form

--- ckanext/datapackage_creator/public/datapackage_creator/js/package_form.js
+++ ckanext/datapackage_creator/public/datapackage_creator/js/package_form.js
@@ -2,24 +2,30 @@ var app = new Vue({
     delimiters: ['[[', ']]'],
     el: '#vue-app',
     data: {
+        contributor_index: 2,
+        extra_index: 0,
         form: {
             title: '',
-            description: '',
+            notes: '',
             organization: 1,
-            visibility: null,
-            license: null,
+            visibility: 'Private',
+            license: '',
             type: 'Tabular',
             source: '',
             version: '',
             tags: '',
             contributors: [
                 {
+                    index: 1,
+                    canDelete: false,
                     type: 'Publisher',
                     name: '',
                     email: '',
                     url: ''
                 },
                 {
+                    index: 2,
+                    canDelete: false,
                     type: 'Author',
                     name: '',
                     email: '',
@@ -27,14 +33,10 @@ var app = new Vue({
                 }
             ],
             frequency: '',
-            extras: [
-                {
-                    key: '',
-                    value: ''
-                }
-            ]
+            extras: []
         },
         contributorTypeOptions: [
+            '',
             'Publisher',
             'Author',
             'Contributor',
@@ -46,9 +48,57 @@ var app = new Vue({
             'Public'
         ],
         licenseOptions: [
-            'No',
-            'MIT',
-            'GPL'
+            {
+                value: '', text: 'Please select the license',
+            },
+            {
+                value: "cc-by", text: "Creative Commons Atribuição"
+            },
+            {
+                value: "cc-by", text: "Creative Commons Atribuição"
+            },
+            {
+                value: "cc-by-sa", text: "Creative Commons Atribuição e Compartilhamento pela mesma Licença"
+            },
+            {
+                value: "cc-zero", text: "Creative Commons CCZero"
+            },
+            {
+                value: "cc-nc", text: "Creative Commons Não-Comercial (Qualquer)"
+            },
+            {
+                value: "odc-odbl", text: "Licença Aberta para Bases de Dados (ODbL) do Open Data Commons"
+            },
+            {
+                value: "gfdl", text: "Licença GNU para Documentação Livre"
+            },
+            {
+                value: "odc-by", text: "Licença de Atribuição do Open Data Commons"
+            },
+            {
+                value: "odc-pddl", text: "Licença e Dedicação ao Domínio Público do Open Data Commons (PDDL)"
+            },
+            {
+                value: "notspecified", text: "Licença não especificada"
+            },
+            {
+                value: "uk-ogl", text: "Open Government Licence do Reino Unido (OGL)"
+            },
+            {
+                value: "other-open", text: "Outra (Aberta)"
+            },
+            {
+                value: "other-at", text: "Outra (Atribuição)"
+            },
+            {
+                value: "other-pd", text: "Outra (Domínio Público)"
+            },
+            {
+                value: "other-closed", text: "Outra (Não Aberta)"
+            },
+            {
+                value: "other-nc", text: "Outra (Não-Comercial)"
+            }
         ],
         organizationOptions: [
             {
@@ -113,11 +163,32 @@ var app = new Vue({
     },
     methods: {
         addContributor() {
+            this.contributor_index += 1
             this.form.contributors.push({
+                index: this.contributor_index,
+                canDelete: true,
                 name: '',
-                type: 'Contributor',
+                type: '',
                 email: '',
                 url: ''
+            })
+        },
+        deleteContributor(contributor) {
+            this.form.contributors = this.form.contributors.filter(function(value, index, arr){
+                return value.index != contributor.index
+            })
+        },
+        addExtra() {
+            this.extra_index += 1
+            this.form.extras.push({
+                index: this.extra_index,
+                key: '',
+                value: ''
+            })
+        },
+        deleteExtra(extra) {
+            this.form.extras = this.form.extras.filter(function(value, index, arr){
+                return value.index != extra.index
             })
         },
         submit() {
